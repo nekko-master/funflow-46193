692c30fbc6939d3bc5e04f5645c6a34f32b16354
nekko-master <kanomaster00@gmail.com>
2025-12-16 17:26:24 +0900

    カンマ抜け

--- app/models/action_type.rb
+++ app/models/action_type.rb
@@ -4,12 +4,12 @@ class ActionType < ActiveHash::Base
   { id: 2, name: 'パレード/ショーを観る' },
   { id: 3, name: '食事' },
   { id: 4, name: 'DPA取得' },
-  { id: 5, name: 'PP取得' }
-  { id: 6, name: 'エントリー受付' }
+  { id: 5, name: 'PP取得' },
+  { id: 6, name: 'エントリー受付' },
   { id: 7, name: 'モバイルオーダー' }
  ]
 
  include ActiveHash::Associations
  has_many :plan_steps
- 
+
 end
