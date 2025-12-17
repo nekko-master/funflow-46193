aa6f7bf052828848f3d91b0e51777a4022518cb9
nekko-master <kanomaster00@gmail.com>
2025-12-17 10:27:14 +0900

    ⑤ プラン作成機能（親：plans）

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,7 +2,7 @@ class PlansController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @plans = current_user.plans
+    @plans = current_user.plans.order(date: :asc)
   end
 
   def new
